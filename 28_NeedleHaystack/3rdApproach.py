1bcd15308652cad18a283bf2f2e2e7d7b776ce8c
Jose Azuara <joseag312@gmail.com>
2019-08-27 12:23:10 -0700

    Added python string slicing approach for problem 28

--- 28_NeedleHaystack/3rdApproach.py
+++ 28_NeedleHaystack/3rdApproach.py
@@ -1,5 +1,6 @@
 # Idea from Billow_jt:
 # Why check for each letter? Python has the ability to use slicing in strings.
+# NOTE: If you're using slicing please remember last element not inclusive!
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
@@ -7,25 +8,25 @@ class Solution:
         len_needle = len(needle)
         len_haystack = len(haystack)
 
+
         if len_needle == 0:
             return(0)
 
-        for i in range(len_haystack):
-            if (i + len_needle) > len_haystack:
-                break
-            elif needle[0] == haystack[i]:
-                for j in range(1, len_needle):
-                    if needle[j] != haystack[i+j]:
-                        break
-                else:
-                    return(i)
+        if needle == haystack:
+            return(0)
+
+        for i in range(len_haystack - len_needle + 1):
+            comp = haystack[i:i+len_needle]
+            if needle == haystack[i:i + len_needle]:
+                return i
 
         return(-1)
+    
 
-#              1        2       3         4       5    6    7       8
-testcase = ['hello', 'Great', 'great', 'aaaaa', 'aaa', '', 'a', 'mississippi']
-#             1     2     3     4     5     6   7       8
-testcase2 = ['ll', 'gr', 'gr', 'b', 'aaa', 'a', '', 'issip'] 
+#              1        2       3         4       5    6    7       8               9               10
+testcase = ['hello', 'Great', 'great', 'aaaaa', 'aaa', '', 'a', 'mississippi', 'mississippi', 'mississippi']
+#             1     2     3     4     5     6   7       8      9    10
+testcase2 = ['ll', 'gr', 'gr', 'b', 'aaa', 'a', '', 'issip', 'pi', 'ti'] 
 mysol = Solution()  
 for i in range(len(testcase)):
     print(mysol.strStr(testcase[i], testcase2[i]))
