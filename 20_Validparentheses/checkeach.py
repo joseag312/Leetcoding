8f48c773e5d8f1380ec68aba5d63252675e16945
Jose Azuara <joseag312@gmail.com>
2019-11-02 13:58:38 -0700

    Stack implementation using built-in list

--- 20_Validparentheses/checkeach.py
+++ 20_Validparentheses/checkeach.py
@@ -1,15 +1,18 @@
-s = r'([)]'
-my_stack = []
+def testing():
+    s = r'([)]'
+    my_stack = []
 
-for i, c in enumerate(s):
-    my_stack += ']' if c == '[' else ''
-    my_stack += '}' if c == '{' else ''
-    my_stack += ')' if c == '(' else ''
+    for i, c in enumerate(s):
+        my_stack += ']' if c == '[' else ''
+        my_stack += '}' if c == '{' else ''
+        my_stack += ')' if c == '(' else ''
 
-    if c  == ']' or c == ')' or c == '}':
-        if my_stack[-1] == c:
-            my_stack.pop()
-        else:
-            print(False)
-else:
-    print(True)
+        if c  == ']' or c == ')' or c == '}':
+            if my_stack[-1] == c:
+                my_stack.pop()
+            else:
+                return (False)
+    else:
+        return(True)
+
+print (testing())
