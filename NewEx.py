93cd0bf4e1381281f77239acc4436feebd97d4d0
Jose Azuara <joseag312@gmail.com>
2019-12-11 14:04:29 -0800

    Set files up to date to resume Leetcoding

--- NewEx.py
+++ NewEx.py
@@ -1,14 +1,30 @@
 from typing import List
 
 class Solution:
-    def name_of_function(self):
+    def reverseList(self, head: ListNode) -> ListNode:
 
-        
 
-            
+
         return([-1,-1])
 
-testcase = {'Fill in test cases'}
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n5 = ListNode(5)
+
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+
+testcase = n1
 mysol = Solution()  
 for i in testcase:
     print(mysol.name_of_function('Arguments'))
