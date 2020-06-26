44397bf80fcda6107250e6099a3907eca07963d8
Jose Azuara <joseag312@gmail.com>
2020-06-25 23:57:22 -0700

    Ex 52 Done!

--- 52_MaxSubarray/2ndApproach.py
+++ 52_MaxSubarray/2ndApproach.py
@@ -12,7 +12,7 @@ class Solution:
         dp = [0]*len(nums)
         dp[0] = nums[0]
         for i in range(1,len(nums)):
-            dp[i] = max(dp[i-1] +nums[i],nums[i])
+            dp[i] = max(dp[i-1] + nums[i],nums[i])
         return max(dp)
 
 testcase = [[-2,1,-3,4,-1,2,1,-5,4]]
--- NewEx.py
+++ NewEx.py
@@ -1,15 +1,27 @@
 from typing import List
 
 class Solution:
-    def reverseList(self, head: ListNode) -> ListNode:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        leng = len(nums) - 1
+        while i <  leng:
+            
+            if nums[i] == 0:
+                j = i + 1
+                while j < (leng + 1):
+                    if nums[j] != 0:
+                        nums[i], nums[j] = nums[j], nums[i]
+                        break
+                    j += 1
+                    
+            i += 1
 
 
-
-        return([-1,-1])
-
-
-testcase = 'Fill out testcases.'
+testcase = [0,1,0,3,12]
 mysol = Solution()  
 for i in testcase:
-    print(mysol.name_of_function('Arguments'))
+    print(mysol.moveZeroes(testcase))
     
